c3675cd58ef8a69b3f9c4612897ab4a351f4785a
Pranisha4841 <pransamrzn@gmail.com>
2026-02-08 19:29:18 +0545

    Push latest changes

--- script.js
+++ script.js
@@ -3,31 +3,25 @@ const button = document.getElementById("button");
 const groceryList = document.getElementById("groceryList");
 
 button.addEventListener("click", function () {
-  const item = input.value.trim();
+  const item = input.value;
   if (item !== "") {
     const li = document.createElement("li");
-
-    const textSpan = document.createElement("span");
-    textSpan.textContent = item;
+    li.textContent = item;
 
     const edit = document.createElement("button");
     edit.textContent = "Edit";
-    edit.className = "edit";
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
-    deleteBtn.className = "delete";
     deleteBtn.onclick = function () {
       li.remove();
     };
 
-    li.appendChild(textSpan);
     li.appendChild(edit);
     li.appendChild(deleteBtn);
 
     groceryList.appendChild(li);
 
     input.value = "";
-    input.focus();
   }
 });
